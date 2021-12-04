6506e7bcda4defe1952e5b080085787c59c86f32
Artur Andrade <adsa@icomp.ufam.edu.br>
2021-12-03 20:42:15 -0400

    changes

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,15 @@ const routes = express.Router();
 
 const movieService = require('./services/movieService');
 
+routes.get('/a', (req, res) => {
+    
+
+
+
+    return res.json({
+        aluno:"etset"
+    })
+})
 routes.get('/movies', movieService.index);
 routes.get('/movies/:name', movieService.show);
 
